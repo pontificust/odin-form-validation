448c0d8f6c236da359dcbfda5a689b4ab6882183
Pontificust <lihtinmann88@yandex.ru>
2026-04-03 15:11:26 +0300

    feat: implement specific error messages

--- src/modules/EventHanlder.js
+++ src/modules/EventHanlder.js
@@ -7,122 +7,126 @@ export class EventHandler {
     this.postalInput = document.querySelector('[data-id="postal"]');
     this.passwordInput = document.querySelector('[data-id="password"]');
     this.confirmInput = document.querySelector('[data-id="confirm"]');
-
-    this.input = {
-      email: () => {
-        if (this.isEmail()) {
-          this.emailInput.nextElementSibling.textContent = "";
-        }
+    this.errorMsg = {
+      email: {
+        typeMismatch: "Wrong type",
+        valueMissing: "Value missing",
       },
-      country: () => {
-        if (this.isCountry()) {
-          this.countryInput.nextElementSibling.textContent = "";
-        }
+      country: {
+        typeMismatch: "The country must exist",
+        valueMissing: "Value missing",
       },
-      postal: () => {
-        if (this.isPostalCode()) {
-          this.postalInput.nextElementSibling.textContent = "";
-        }
+      postal: {
+        typeMismatch: "The code doesn't exist",
+        valueMissing: "Value missing",
       },
-      password: () => {
-        if (this.isPassword) {
-          this.passwordInput.nextElementSibling.textContent = "";
-        }
+      password: {
+        typeMismatch: `The password must contain al least:
+        - one uppercase English letter
+        - one lowercase English letter
+        - one digit
+        - one special character.`,
+        valueMissing: "Value missing",
       },
-      confirm: () => {
-        if (this.isConfirm()) {
-          this.confirmInput.nextElementSibling.textContent = "";
-        }
+      confirm: {
+        typeMismatch: "The passwords must be equal",
+        valueMissing: "Value missing",
       },
     };
 
+    this.input = {
+      email: () => this.inputHandler(this.isEmail, this.emailInput),
+      country: () => this.inputHandler(this.isCountry, this.countryInput),
+      postal: () => this.inputHandler(this.isPostalCode, this.postalInput),
+      password: () => this.inputHandler(this.isPassword, this.passwordInput),
+      confirm: () => this.inputHandler(this.isConfirm, this.confirmInput),
+    };
+
     this.focusout = {
-      email: () => {
-        if (this.isEmail()) {
-          this.emailInput.nextElementSibling.textContent = "";
-        } else {
-          this.emailInput.nextElementSibling.textContent =
-            "Input an email addres in correct format";
-        }
-      },
-      country: () => {
-        if (this.isCountry()) {
-          this.countryInput.nextElementSibling.textContent = "";
-        } else {
-          this.countryInput.nextElementSibling.textContent =
-            "Input an email addres in correct format";
-        }
-      },
-      postal: () => {
-        if (this.isPostalCode()) {
-          this.postalInput.nextElementSibling.textContent = "";
-        } else {
-          this.postalInput.nextElementSibling.textContent = "Error";
-        }
-      },
-      password: () => {
-        if (this.isPassword()) {
-          this.passwordInput.nextElementSibling.textContent = "";
-        } else {
-          this.passwordInput.nextElementSibling.textContent = "Error";
-        }
-      },
-      confirm: () => {
-        if (this.isConfirm()) {
-          this.confirmInput.nextElementSibling.textContent = "";
-        } else {
-          this.confirmInput.nextElementSibling.textContent = "Error";
-        }
-      },
+      email: () => this.focusoutHandler(this.isEmail, this.emailInput),
+      country: () => this.focusoutHandler(this.isCountry, this.countryInput),
+      postal: () => this.focusoutHandler(this.isPostalCode, this.postalInput),
+      password: () => this.focusoutHandler(this.isPassword, this.passwordInput),
+      confirm: () => this.focusoutHandler(this.isConfirm, this.confirmInput),
     };
 
     this.submit = {
       form: (e) => {
-
-        if(!this.isEmail() || !this.isPostalCode() || !this.isConfirm()){
-
+        if (!this.isEmail() || !this.isPostalCode() || !this.isConfirm()) {
           e.preventDefault();
         }
       },
     };
   }
 
-  isPostalCode() {
-    const countryName = this.capitalizeStr(this.countryInput.value);
+  isPostalCode = () => {
+    let countryName;
+    if(!this.isCountry()){
+      return false;
+    } else {
+      countryName = this.capitalizeStr(this.countryInput.value);
+    }
     const postalRegExp = new RegExp(
       this.postalCodes[this.countries[countryName]],
     );
-    console.log(postalRegExp);
     const validity =
       countryName.length !== 0 &&
       postalRegExp.test(this.postalInput.value) &&
       this.isCountry();
     return validity;
-  }
+  };
 
-  isEmail() {
+  isEmail = () => {
     return this.emailInput.validity.valid;
-  }
+  };
 
-  isCountry() {
+  isCountry = () => {
     return (
       this.countryInput.validity.valid &&
       Object.hasOwn(this.countries, this.capitalizeStr(this.countryInput.value))
     );
-  }
+  };
 
-  isPassword() {
+  isPassword = () => {
     return this.passwordInput.validity.valid;
-  }
+  };
 
-  isConfirm() {
+  isConfirm = () => {
     return (
       this.confirmInput.value === this.passwordInput.value && this.isPassword()
     );
+  };
+
+  inputHandler(validator, input) {
+    if (validator()) {
+      input.nextElementSibling.textContent = "";
+    }
   }
 
-  capitalizeStr(str) {
+  focusoutHandler(validator, input) {
+    const errorSpan = input.nextElementSibling;
+    if (validator()) {
+      errorSpan.textContent = "";
+    } else {
+      this.showError(input, errorSpan);
+    }
+  }
+
+  findErrorMsg(input) {
+    const inputName = input.dataset.id;
+    const errorMsg = input.validity.valueMissing
+      ? this.errorMsg[inputName].valueMissing
+      : this.errorMsg[inputName].typeMismatch;
+    return errorMsg;
+  }
+
+  showError(input, errorSpan) {
+    const errorMsg = this.findErrorMsg(input);
+    errorSpan.textContent = errorMsg;
+  }
+
+  capitalizeStr = (str) => {
     const [first, ...rest] = str.split("");
     return first.toUpperCase() + rest.join("");
-  }
+  };
 }
